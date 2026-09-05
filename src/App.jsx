b8fd71fea491ac7400d17d88f062d80f310121e9
andrew0samir <andrewsamir317@gmail.com>
2026-09-05 16:32:54 +0300

    implement the finder window

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
 import { Dock, Navbar, Welcome } from "@components";
-import { Resume, Safari, Terminal } from "@windows";
+import { Finder, Resume, Safari, Terminal } from "@windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -15,6 +15,7 @@ function App() {
       <Terminal />
       <Safari />
       <Resume />
+      <Finder />
     </main>
   );
 }
